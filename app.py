e101739a0ae142e800fa6f70cce7d8a5c5277329
Passkou <psk116@outlook.com>
2020-10-20 21:41:53 +0800

    1

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+import time
+import threading
+import json
+import hmac
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if (request.headers.get('X-GitHub-Event', '') == 'push'):
+        os.startfile('./pull.bat')
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
